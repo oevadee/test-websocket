c88f27d224f7b586eb0fcdfc90851738caf23d7d
MR dev <oevadee@Adrians-MacBook-Pro.local>
2021-12-22 22:20:37 +0100

    chore(backend): remove node_mobules...

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-const io = require('socket.io')(3000)
-io.on('connection', socket => {
-    console.log(socket.id)
-})
+const io = require('socket.io')(3000);
+
+io.on('connection', (socket) => {
+  console.log(socket.id);
+});
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,4 +12,4 @@ export const App = () => (
       </a>
     </p>
   </>
-)
+);
